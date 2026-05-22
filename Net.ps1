f523d05557ba974872a01db914d00f58e2e09344
Mustafa Merchant <74061610+BlueStreak79@users.noreply.github.com>
2026-05-22 09:38:49 +0530

    Update Net.ps1

--- Net.ps1
+++ Net.ps1
@@ -454,45 +454,24 @@ function MainMenu {
         Write-Host "7  - Proxy Reset"
         Write-Host "8  - Restart Adapters"
         Write-Host "9  - Full Network Stack Reset"
-
-        Write-Host ""
-
         Write-Host "10 - Google DNS"
         Write-Host "11 - Cloudflare DNS"
         Write-Host "12 - Quad9 DNS"
         Write-Host "13 - Restore DHCP DNS"
-
-        Write-Host ""
-
         Write-Host "14 - Show Wi-Fi Profiles"
         Write-Host "15 - Delete All Wi-Fi Profiles"
         Write-Host "16 - Delete Single Wi-Fi Profile"
-
-        Write-Host ""
-
         Write-Host "17 - Run SFC"
         Write-Host "18 - Run DISM"
-
-        Write-Host ""
-
         Write-Host "19 - TCP Optimization"
-
-        Write-Host ""
-
         Write-Host "20 - Connectivity Test"
         Write-Host "21 - Ping Test"
         Write-Host "22 - Trace Route"
         Write-Host "23 - DNS Lookup"
         Write-Host "24 - Public IP"
         Write-Host "25 - Adapter Information"
-
-        Write-Host ""
-
         Write-Host "26 - Ookla Speed Test"
         Write-Host "27 - Speed Test JSON Export"
-
-        Write-Host ""
-
         Write-Host "0  - Exit"
 
         Write-Host ""
@@ -510,28 +489,22 @@ function MainMenu {
             7  { Reset-Proxy }
             8  { Restart-NetworkAdapters }
             9  { Full-NetworkReset }
-
             10 { Set-GoogleDNS }
             11 { Set-CloudflareDNS }
             12 { Set-Quad9DNS }
             13 { Restore-DHCPDNS }
-
             14 { Show-WifiProfiles }
             15 { Delete-AllWifiProfiles }
             16 { Delete-SingleWifiProfile }
-
             17 { Run-SFC }
             18 { Run-DISM }
-
             19 { TCP-Optimization }
-
             20 { Connectivity-Test }
             21 { Ping-Test }
             22 { Trace-Test }
             23 { DNS-Lookup }
             24 { Public-IP }
             25 { Adapter-Info }
-
             26 { Speed-Test }
             27 { Speed-TestJSON }
 
