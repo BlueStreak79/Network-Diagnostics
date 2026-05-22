0fe4b8c3b29d9187ae8605dd6f87234ef0ea6475
Mustafa Merchant <74061610+BlueStreak79@users.noreply.github.com>
2026-05-22 09:01:48 +0530

    Update Net.ps1

--- Net.ps1
+++ Net.ps1
@@ -4,9 +4,7 @@
 # ==============================
 
 # Auto Elevation
-If (-NOT ([Security.Principal.WindowsPrincipal]
-[Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(
-[Security.Principal.WindowsBuiltInRole] "Administrator")) {
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
 
     Start-Process powershell "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     Exit
